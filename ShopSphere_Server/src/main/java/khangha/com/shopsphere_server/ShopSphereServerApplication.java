934f8b7249b8c2536de970a3032e1ed8c40e3048
KhangHa-Automate <danny.dada.hak.@gmail.com>
2025-08-23 18:20:03 +0700

    SS-init

---
+++ ShopSphere_Server/src/main/java/khangha/com/shopsphere_server/ShopSphereServerApplication.java
@@ -0,0 +1,13 @@
+package khangha.com.shopsphere_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopSphereServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopSphereServerApplication.class, args);
+    }
+
+}
---
+++ ShopSphere_Server/src/test/java/khangha/com/shopsphere_server/ShopSphereServerApplicationTests.java
@@ -0,0 +1,13 @@
+package khangha.com.shopsphere_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopSphereServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
